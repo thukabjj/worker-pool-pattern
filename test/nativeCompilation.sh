753132b37f445a7d6d09413ddb590db3c6bcd9a5
Arthur Alves <arthur.alvesdeveloper@outlook.com>
2023-05-29 00:16:16 +0100

    wip: Finalizing the example with Golang and Java with GraalVM

--- test/nativeCompilation.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-javac -d build src/main/java/com/github/thukabjj/javaworkerpool/WorkerPoolPattern.java
-
-jar cfvm WorkerPoolPattern.jar META-INF/MANIFEST.MF -C build .
-
-native-image -jar WorkerPoolPattern.jar
